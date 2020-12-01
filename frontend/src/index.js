c48648b977d22de6411b90f76d7f901a788e0d10
Denis Lesnik <blackgod@inbox.ru>
2020-12-01 17:43:54 +0100

    Update index.js
    Since the 2020.2 it's allowed to pass two PlaceIds (one for the Sakura UI, another for the Classic UI)

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@
 import {Plugin, React} from "@jetbrains/teamcity-api"
 import Chart from './Chart/Chart'
 
-new Plugin(Plugin.placeIds.SAKURA_BUILD_LINE_EXPANDED, {
+new Plugin([Plugin.placeIds.SAKURA_BUILD_LINE_EXPANDED, 'BUILD_RESULTS_FRAGMENT'], {
     name: "LocustioChart-Plugin",
     content: Chart,
 });
